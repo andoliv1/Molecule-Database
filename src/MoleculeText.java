3fc4d0ea1ef51030457b0342c28e1a0a91319a4a
Timmy Hoang <hoangt@bu.edu>
2020-03-17 22:35:14 -0400

    MoleculeText: extend from MoleculeAbstract. Create getter functions. Add adjacency matrix.

--- src/MoleculeText.java
+++ src/MoleculeText.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 
-public class MoleculeText {
-    String moleculeName;
-    Integer numVertices;
-    String[] atoms;
-    LinkedList<Integer>[] adjacencyList;
+public class MoleculeText extends MoleculeAbstract{
 
     public MoleculeText(String filename){
         parseFile(filename);
@@ -26,15 +23,17 @@ public class MoleculeText {
             this.numVertices = Integer.parseInt(br.readLine());
             System.out.println(numVertices);
             // The next numVertices line are the atoms in the molecule
-            this.atoms = new String[this.numVertices];
+            this.atoms = new ArrayList<>(this.numVertices);
             this.adjacencyList = new LinkedList[this.numVertices];
             for (int ii = 0; ii<this.numVertices; ii++){
-                this.atoms[ii] = br.readLine();
-                System.out.println(this.atoms[ii]);
+                this.atoms.add(br.readLine());
+                System.out.println(this.atoms.get(ii));
                 this.adjacencyList[ii] = new LinkedList<>();
             }
 
             // Remainder of file is the adjacency list
+            // init adjacency matrix nxn
+            this.adjacencyMatrix = new int[this.numVertices][this.numVertices];
             String st;
             String[] delimited;
             int vertex1, vertex2;
@@ -44,6 +43,8 @@ public class MoleculeText {
                 vertex2 = Integer.parseInt(delimited[1]);
 
                 this.adjacencyList[vertex1].add(vertex2);
+                this.adjacencyMatrix[vertex1][vertex2] += 1;
+                this.adjacencyMatrix[vertex2][vertex1] += 1;
             }
             System.out.println("Adjacency List");
             for (int ii = 0; ii<this.numVertices; ii++){
@@ -53,8 +54,42 @@ public class MoleculeText {
                 }
                 System.out.println();
             }
+
+            System.out.println("Adjacency Matrix");
+            for (int ii = 0; ii<this.numVertices; ii++){
+                for (int jj = 0; jj<this.numVertices; jj++){
+                    System.out.print(adjacencyMatrix[ii][jj] +"\t");
+                }
+                System.out.println();
+            }
         } catch(IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public String getMoleculeName(){
+        return moleculeName;
+    }
+
+    public Integer getNumVertices(){
+        return numVertices;
+    }
+
+    public ArrayList<String> getAtomList(){
+        return atoms;
+    }
+
+    public LinkedList<Integer>[] getAdjacencyList(){
+        return adjacencyList;
+    }
+
+    public int[][] getAdjacencyMatrix(){
+        return adjacencyMatrix;
+    }
+
+    public static void main(String[] args) {
+        MoleculeText m = new MoleculeText("butane.txt");
+
     }
 }
