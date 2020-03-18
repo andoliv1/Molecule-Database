9521c081aa2d4377e639f6226e55e9e59c650c5a
Timmy Hoang <hoangt@bu.edu>
2020-03-17 22:33:58 -0400

    Rename moleculeAbstract to MoleculeAbstract. Set class variables to be protected.

---
+++ src/MoleculeAbstract.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public abstract class MoleculeAbstract {
+    protected String moleculeName;
+    protected Integer numVertices;
+    protected ArrayList<String> atoms;
+    protected LinkedList<Integer>[] adjacencyList;
+    protected int[][] adjacencyMatrix;
+
+    public abstract String getMoleculeName();
+    public abstract Integer getNumVertices();
+    public abstract LinkedList<Integer>[] getAdjacencyList();
+    public abstract int[][] getAdjacencyMatrix();
+    public abstract ArrayList<String> getAtomList();
+}
--- src/moleculeAbstract.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public abstract class moleculeAbstract {
-    public abstract int[][] getAdjacencyMatrix();
-
-    public abstract ArrayList<String> getAtomList();
-
-    public abstract Object convertToFormat(String encoding);
-}
