ccdd4ecb7fe1e3c79ef3fadcb80c914eb060371b
andoliv1 <andoliv@bu.edu>
2020-03-18 10:10:27 -0300

    implementedDumbSearch

--- src/Search.java
+++ src/Search.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 public interface Search {
 
     //This method should return the section of our SQL Database that the molecule can be in
-    public int[] findIDsSql(ArrayList<String> adjacencyList);
+    public static int[] findIDsSql(ArrayList<String> adjacencyList) {
+        return new int[0];
+    }
 
     //this method should return whether the two molecules are isomorphic
-    public boolean isIsomorphic(moleculeChemSpider molecule1, moleculeChemSpider molecule2);
+    public static boolean isIsomorphic(moleculeChemSpider molecule1, moleculeChemSpider molecule2) {
+        return false;
+    }
 
 }
--- src/searchDumb.java
+++ src/searchDumb.java
@@ -1,6 +1,8 @@
+import javafx.util.Pair;
+
 import java.util.*;
 
-public class searchDumb implements Search {
+public class searchDumb{
 
     private static int compare(String o1, String o2) {
         if ((int) o1.charAt(0) > (int) o2.charAt(0)) {
@@ -24,7 +26,7 @@ public class searchDumb implements Search {
 
     //this will sort the adjacencyList in alphabetical order meaning by first letter and if it contains a second letter
     //by second letter
-    public ArrayList<String> sortAtomList(ArrayList<String> atomList){
+    public static ArrayList<String> sortAtomList(ArrayList<String> atomList){
         Collections.sort(atomList, (Comparator<String>) searchDumb::compare);
         return atomList;
     }
@@ -36,9 +38,7 @@ public class searchDumb implements Search {
     search for.
     Comment: I am not quite sure how to query the SQL database yet so I need to figure out how to do this.
      */
-
-    @Override
-    public int[] findIDsSql(ArrayList<String> adjacencyList) {
+    public static int[] findIDsSql(ArrayList<String> adjacencyList) {
         return new int[0];
     }
 
@@ -47,14 +47,67 @@ public class searchDumb implements Search {
     that still satisfy the molecule arrangement and check if any of those matrices match the isomers we found in the
     findIDsSql method. This is an n! step for each isomer we have.
      */
-    @Override
-    public boolean isIsomorphic(moleculeChemSpider molecule1, moleculeChemSpider molecule2) {
+    public static boolean isIsomorphic(moleculeChemSpider molecule1, moleculeChemSpider molecule2) {
         int[][] adj1 = molecule1.getAdjacencyMatrix();
         int[][] adj2 = molecule2.getAdjacencyMatrix();
         ArrayList<String> ato1 = molecule1.getAtomList();
         ArrayList<String> ato2 = molecule2.getAtomList();
+        System.out.println(ato1.toString());
+        //get all the repeating elements because they are the ones that dictate all the different mappings meaning if an
+        //atom only occurs in the molecule once they all we have to do is replace it with the position that it was in the
+        //original database molecule.
+        ArrayList<Pair<Integer,Integer>> mappings = new ArrayList<>();
+        ArrayList<String> duplicates = new ArrayList<>();
+        //note ato1 and ato2 should have the same size
+        int position = 0;
+        boolean found = true;
+        ArrayList<Integer> used = new ArrayList<>();
+        int i = 0;
+        while(found == true && i < ato1.size()) {
+            found = false;
+            int j = 0;
+            while(found == false && j < ato2.size()) {
+                if (ato1.get(i).equals(ato2.get(j)) && (used.contains(j) == false)) {
+                    int[] connections = adj1[i];
+                    int[] connections2 = adj2[j];
+                    ArrayList<String> atom_connections = new ArrayList<>();
+                    ArrayList<String> atom_connections2 = new ArrayList<>();
+                    for (int k = 0; k < connections.length; k++) {
+                        if (connections[k] == 1) {
+                            atom_connections.add(ato1.get(k));
+                        }
+                        if (connections2[k] == 1) {
+                            atom_connections2.add(ato2.get(k));
+                        }
+                    }
+                    sortAtomList(atom_connections);
+                    sortAtomList(atom_connections2);
+                    System.out.println("Here are the atom1 connections " + atom_connections.toString());
+                    System.out.println("Here are the atom2 connections " + atom_connections2.toString());
+                    int w = 0;
+                    while (w < atom_connections.size()) {
+                        if (atom_connections.get(w).equals(atom_connections2.get(w)) == false) {
+                            found = false;
+                        }
+                        w++;
+                    }
+                    System.out.println("w " + w);
+                    System.out.println("atom connections size " + atom_connections.size());
+                    if (w == atom_connections.size()) {
+                        System.out.println("hello");
+                        used.add(j);
+                        found = true;
+                    }
+                }
+                j++;
+            }
+            if(found == false){
+                return false;
+            }
+            i++;
+        }
 
-        return false;
+        return true;
     }
 
     /*
@@ -66,5 +119,33 @@ public class searchDumb implements Search {
      */
     public int[][] matrixPermutation(int[][] adjacencyMatrix){
         return null;
+    }
+
+    public static void main(String[] args){
+        ArrayList<String> water_atoms = new ArrayList<String>();
+        water_atoms.add("H");
+        water_atoms.add("H");
+        water_atoms.add("O");
+        int[][] water_matrix = new int[3][3];
+        water_matrix[0][2] = 1;
+        water_matrix[1][2] = 1;
+        water_matrix[2][0] = 1;
+        water_matrix[2][1] = 1;
+
+        ArrayList<String> water_atom2 = new ArrayList<String>();
+        water_atom2.add("O");
+        water_atom2.add("O");
+        water_atom2.add("H");
+        int[][] water_matrix2 = new int[3][3];
+        water_matrix2[0][2] = 1;
+        water_matrix2[0][1] = 1;
+        water_matrix2[1][0] = 1;
+        water_matrix2[2][0] = 1;
+        moleculeChemSpider water = new moleculeChemSpider(water_matrix,water_atoms);
+        moleculeChemSpider water2 = new moleculeChemSpider(water_matrix2,water_atom2);
+        boolean ok = isIsomorphic(water,water2);
+        System.out.println(ok);
+        System.out.println(Arrays.deepToString(water.getAdjacencyMatrix()));
+        System.out.println(water.getAtomList().toString());
     }
 }
