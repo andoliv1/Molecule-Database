6c884daf7e3d25489785245b95182a47fd78a951
Timmy <timmytuhoang@gmail.com>
2020-04-12 23:02:25 +0000

    Add more unit tests.

--- src/test/SmallSystemsTest.java
+++ src/test/SmallSystemsTest.java
@@ -1,9 +1,6 @@
 package test;
 
-import main.java.H2DB;
-import main.java.MoleculeDB;
-import main.java.MoleculeText;
-import main.java.searchDumb;
+import main.java.*;
 import org.dbunit.DatabaseTestCase;
 import org.dbunit.database.DatabaseConfig;
 import org.dbunit.database.DatabaseConnection;
@@ -25,6 +22,8 @@ public class SmallSystemsTest {
     H2DB h2;
     public IDatabaseConnection connection;
     public DatabaseConfig dbConfig;
+
+
     public SmallSystemsTest() {
         super();
         h2 = new H2DB();
@@ -53,5 +52,60 @@ public class SmallSystemsTest {
 
         boolean strongIso = searchDumb.verify_rigorous_isomorphism(m1, m2);
         assert(strongIso);
+    }
+
+    /**
+     * Database must be populated.
+     * This test will test to see how fast ten inserts are.
+     * @throws Exception
+     */
+    @Test
+    public void InsertTimingTest() throws Exception{
+
+        // 10 Insertions
+        long startTime = System.nanoTime();
+        h2.insertMolecule("carbon_dioxide.txt");
+        h2.insertMolecule("carbon_dioxide2.txt");
+        h2.insertMolecule("carbon_dioxide.txt");
+        h2.insertMolecule("carbon_dioxide2.txt");
+        h2.insertMolecule("carbon_dioxide.txt");
+        h2.insertMolecule("carbon_dioxide2.txt");
+        h2.insertMolecule("carbon_dioxide.txt");
+        h2.insertMolecule("carbon_dioxide2.txt");
+        h2.insertMolecule("carbon_dioxide.txt");
+        h2.insertMolecule("carbon_dioxide2.txt");
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
+        System.out.println(duration + "ms");
+        assert(duration < 1000); // Assert duration is less than 1 second
+
+    }
+
+    /**
+     * Database must be populated.
+     * This test will test to see how fast ten finds are.
+     * @throws Exception
+     */
+    @Test
+    public void FindTimingTest() throws Exception{
+        Operations op = new Operations();
+        // 10 Find
+        long startTime = System.nanoTime();
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+        op.find("carbon_dioxide.txt");
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime)/1000000;
+        System.out.println(duration + "ms");
+        assert(duration < 1000); // Assert duration is less than 1 second
+
     }
 }
