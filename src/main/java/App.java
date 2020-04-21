53eef7f94c1a6b6af5beba63dbef41e382b78fa8
Timmy <timmytuhoang@gmail.com>
2020-04-21 14:44:03 +0000

    Plot two molecules side by side.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,18 +3,25 @@ package main.java;
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.StyledDocument;
+import javax.swing.text.View;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.stream.ProxyPipe;
+import org.graphstream.ui.geom.Point3;
+import org.graphstream.ui.graphicGraph.GraphicGraph;
 import org.graphstream.ui.swingViewer.ViewPanel;
 import org.graphstream.ui.view.Viewer;
-
+import static org.graphstream.algorithm.Toolkit.*;
 
 public class App {
     private static final Logger log = Logger.getLogger( App.class.getName() );
@@ -27,10 +34,12 @@ public class App {
     private JTextPane textPane1;
     private JPanel graphPanel1;
     private JPanel graphPanel2;
+    private JButton button1;
     Operations Ops;
     JFrame frame;
     private String moleculeFile;
-
+    Viewer viewer1;
+    Viewer viewer2;
     public App() {
 
         // Initialize Operations class
@@ -77,8 +86,11 @@ public class App {
                         createGraph(graphPanel1, new MoleculeText(moleculeFile));
                     } else if (findButton.isSelected()) {
                         ArrayList<MoleculeAbstract> molecules = Ops.find(moleculeFile);
+                        textPane1.setText("");
                         StyledDocument doc = textPane1.getStyledDocument();
+
                         if (molecules.size() > 0) {
+                            Graph graph1 = createGraph(graphPanel1, new MoleculeText(moleculeFile));
                             for (MoleculeAbstract m : molecules) {
                                 try {
 
@@ -86,7 +98,13 @@ public class App {
                                 } catch (BadLocationException e) {
                                     e.printStackTrace();
                                 }
-                                createGraph(graphPanel2, m);
+                            }
+                            Graph graph2 = createGraph(graphPanel2, molecules.get(0));
+                        } else{
+                            try {
+                                doc.insertString(doc.getLength(), "No isomorphic molecules found.\n", null);
+                            } catch (BadLocationException e) {
+                                e.printStackTrace();
                             }
                         }
                     }
@@ -94,48 +112,70 @@ public class App {
 
             }
         });
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                setNodePos();
+            }
+        });
 //        addMolecule("molecules/fake_mol1_13atom.txt");
         drawGraph(graphPanel1, new SingleGraph("Empty Graph", false, true));
         drawGraph(graphPanel2, new SingleGraph("Empty Graph", false, true));
 
+
     }
 
-    public void createGraph(JPanel graphPanel, MoleculeAbstract m){
+    public Graph createGraph(JPanel graphPanel, MoleculeAbstract m){
 
-        Graph g = new SingleGraph(m.getMoleculeName(), false, true);
+        Graph g = new MultiGraph(m.getMoleculeName(), false, true);
+        URL url = App.class.getResource("/public/GUIStyle.css");
+        g.addAttribute("ui.stylesheet","url("+url.getFile()+")");
+
+
         for (int vertex = 0; vertex < m.numVertices; vertex++){
+            System.out.println(m.getAtomList().get(vertex));
             String atom = m.getAtomList().get(vertex);
-            Node n = g.addNode( atom + vertex); //C0, H1, ...
+            Node n = g.addNode( ""+vertex); //C0, H1, ...
             n.addAttribute("ui.label", atom);
+            n.addAttribute("ui.color", (float) vertex / m.numVertices);
+            if(vertex < m.numVertices/2) {
+                n.addAttribute("ui.style", "text-color:white;");
+            }
+            else{
+                n.addAttribute("ui.style", "text-color:black;");
+            }
         }
+        System.out.println("-=--------");
+
         for (int vertex1 = 0; vertex1< m.getNumVertices(); vertex1++)
-            for (int j = 0; j < m.getAdjacencyList()[vertex1].size(); j++){
-                int vertex2 = m.getAdjacencyList()[vertex1].get(j);
-                g.addEdge(m.atoms.get(vertex1)+vertex1 + m.atoms.get(vertex2)+vertex2, vertex1, vertex2);
+            for (int vertex2 = 0; vertex2 < m.getNumVertices(); vertex2++) {
+                // no repeats
+                if (vertex1 < vertex2) {
+                    for (int i = 0; i < m.getAdjacencyMatrix()[vertex1][vertex2]; i++)
+                        g.addEdge(m.atoms.get(vertex1) + vertex1
+                                        + m.atoms.get(vertex2) + vertex2
+                                        + "_" + i,
+                                vertex1,
+                                vertex2
+                                );
+                }
             }
 
         drawGraph(graphPanel, g);
+        return g;
     }
-    public void drawGraph(JPanel graphPanel, Graph graph){
-//        Graph graph = new SingleGraph("Tutorial 1", false, true);
-//        Node a = graph.addNode("A");
-//        a.addAttribute("xy", 0, 0);
-//        a.addAttribute("ui.label", "A");
-//        Node b = graph.addNode("B");
-//        b.addAttribute("xy", 10, 0);
-//        b.addAttribute("ui.label", "B");
-//
-//        Node c = graph.addNode("C");
-//        c.addAttribute("xy", 10, 10);
-//        c.addAttribute("ui.label", "C");
-//
-//
-//        graph.addEdge("AB", "A", "B");
-//        graph.addEdge("BC", "B", "C");
-//        graph.addEdge("CA", "C", "A");
-////        graph.display();
+
+    public void drawGraph(JPanel graphPanel, Graph graph) {
         Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_GUI_THREAD);
-        viewer.enableAutoLayout();
+        if (graphPanel == graphPanel2) {
+            viewer.disableAutoLayout();
+            viewer2 = viewer;
+        }
+        else {
+            viewer.enableAutoLayout();
+            viewer1 = viewer;
+        }
         ViewPanel viewPanel = viewer.addDefaultView(false);
         viewPanel.setLayout(new BorderLayout());
         viewPanel.setPreferredSize(new Dimension(800,600));
@@ -143,11 +183,31 @@ public class App {
         graphPanel.removeAll();
         graphPanel.add(viewPanel, BorderLayout.CENTER);
         graphPanel.revalidate();
-        System.out.println(viewPanel);
     }
 
+    public boolean setNodePos(){
+        if(viewer1 == null || viewer2 == null)
+            return false;
+        GraphicGraph graph1 = viewer1.getGraphicGraph();
+        GraphicGraph graph2 = viewer2.getGraphicGraph();
+
+        if(graph1.getNodeCount() != graph2.getNodeCount())
+            return false;
+
+        for(int i = 0; i < graph1.getNodeCount(); i++) {
+            Node n1 = graph1.getNode(""+i);
+            Node n2 = graph2.getNode(""+i);
+
+            double[] xy = nodePosition(n1);
+            System.out.println(xy[0] + "   " + xy[1]);
+            n2.addAttribute("xy", xy[0], xy[1]);
+        }
+//        drawGraph(graphPanel2, graph2);
+        return true;
+    }
 
     public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
         JFrame frame = new JFrame("App");
         frame.setContentPane(new App().mainPanel);
 //        frame.add(mainPanel);
